eb1aa61bf151d91a4adaae8ac58f5e98dd907a3a
Amish-03 <163165606+Amish-03@users.noreply.github.com>
2024-06-11 22:55:56 +0530

    Time: 80 ms (22.65%), Space: 14.1 MB (28.92%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.c
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.c
@@ -1,13 +1,15 @@
 int findDuplicate(int* nums, int numsSize) {
      int *A = (int*)calloc(numsSize, sizeof(int));
-    
+        int dup;
     for(int i=0;i<numsSize;i++)
     {
         A[nums[i]]++;
         if(A[nums[i]]>1)
         {
-            return nums[i];
+            dup= nums[i];
+            break;
         }
     }
-    return -1;
+    free(A);
+    return dup;
 }
