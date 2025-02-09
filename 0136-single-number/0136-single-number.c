1bc6edcb59406c1da16ae3a611a69f89a39a4ea8
Amish-03 <163165606+Amish-03@users.noreply.github.com>
2025-02-09 17:10:57 +0530

    Time: 3 ms (20.35%), Space: 9.7 MB (7.18%) - LeetHub

--- 0136-single-number/0136-single-number.c
+++ 0136-single-number/0136-single-number.c
@@ -1,12 +1,33 @@
 int singleNumber(int* nums, int numsSize) 
 {
-    int xor=0;
-    int i;
+    int i,max1=nums[0],offset=30000;
     for(i=0;i<numsSize;i++)
     {
-        xor^=nums[i];
+        max1=(max1>nums[i])?max1:nums[i];
     }
-    return xor;
+
+    long long freq[60001];
+
+    for(i=0;i<60001;i++)
+    {
+        freq[i]=0;
+    }
+
+    for(i=0;i<numsSize;i++)
+    {
+        freq[nums[i]+offset]++;
+    }
+    int val=0;
+    for(i=0;i<60001;i++)
+    {
+        if(freq[i]==1)
+        {
+            val=i-offset;
+            break;
+        }
+    }
+
+    return val;
     
     
 }
