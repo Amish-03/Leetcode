dde303d346f5802a68d26821f0b0b79122e48b46
Amish-03 <163165606+Amish-03@users.noreply.github.com>
2025-02-15 13:49:12 +0530

    Time: 0 ms (100%), Space: 10.2 MB (27.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        if(s.size()==0)
-            return 1;
-        string s1,s2;
-        int i;
-        for(i=0;s[i]!='\0';i++)
+    bool isPalindrome(string s) 
+    {
+        string new1;
+        for(int i=0;s[i]!='\0';i++)
         {
             if(isalnum(s[i]))
-            {
-                s1.push_back(tolower(s[i]));
-                
-            }
-            
-        }
-        cout<<s1<<endl;
-        s2=s1;
-        int start=0;
-        int end=s2.size()-1;
-        char temp;
-        while(start<end)
+            new1.push_back(tolower(s[i]));
+        }  
+        cout<<new1;
+        int flag=1;
+        int size=new1.size();
+        for(int i=0;new1[i]!='\0';i++)
         {
-            temp=s2[start];
-            s2[start]=s2[end];
-            s2[end]=temp;
-
-            start++;
-            end--;
+            if(new1[i]!=new1[(size-1)-i])
+                flag=0;
         }
 
-        if(s1==s2)
-            return 1;
-        else
-            return 0;
-        
+        return flag;
     }
 };
