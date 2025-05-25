f4267edf1c5f289de317cebaf4a6995090b775fd
Amish-03 <163165606+Amish-03@users.noreply.github.com>
2025-05-25 22:38:35 +0530

    Time: 2 ms (71.04%), Space: 8 MB (10.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -3,10 +3,13 @@
 int fib(int n){
     if(n==0)
         return 0;
-    if(n<3)
+    int a[100];
+    a[0]=1;
+    a[1]=1;
+    
+    for(int i=2;i<n;i++)
     {
-        return 1;
+        a[i]=a[i-1]+a[i-2];
     }
-
-    return fib(n-1) + fib(n-2);
+    return a[n-1];
 }
