d8534bbc3928decbe9627fa83a30f7600577c2d0
Amish Bhushan Kulkarni <163165606+Amish-03@users.noreply.github.com>
2025-07-09 12:13:48 +0530

    Time: 629 ms (13.9%), Space: 171.5 MB (7.74%) - LeetHub

---
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.cpp
@@ -0,0 +1,24 @@
+#include<math.h>
+class Solution {
+public:
+    vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        int n=queries.size();
+        int k=points.size();
+        vector<int> ans(n);
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            count=0;
+            vector<int> curCircle= queries[i];
+            for(int j=0;j<k;j++)
+            {
+                vector<int> curPoint=points[j];
+                double curDist = sqrt((curCircle[0]-curPoint[0])*(curCircle[0]-curPoint[0]) + (curCircle[1]-curPoint[1])*     (curCircle[1]-curPoint[1]));
+                if(curDist<=curCircle[2])
+                    count++;
+            }
+            ans[i]=count;
+        }
+        return ans;
+    }
+};
