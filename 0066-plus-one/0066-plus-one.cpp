8788525a0957ab7fbefcfcac4a172e45b079e58b
Amish Bhushan Kulkarni <163165606+Amish-03@users.noreply.github.com>
2025-07-22 17:54:10 +0530

    Time: 0 ms (100%), Space: 11.6 MB (34.86%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        int c=1;  
+
+        for (int i=n-1;i>=0;i--) 
+        {
+            int sum=digits[i]+c;
+            digits[i]=sum%10;
+            c=sum/10;
+            if (c==0)
+                break;
+        }
+
+        if(c)
+            digits.insert(digits.begin(),1);
+
+        return digits;
+
+    }
+};
