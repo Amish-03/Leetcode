901f97a1bc1d4b86194f232f4c72f858d82b198f
Amish Bhushan Kulkarni <163165606+Amish-03@users.noreply.github.com>
2025-08-29 20:18:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (67.4%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,15 +7,20 @@ public:
         else if(n==2)
             return (nums[0]>nums[1])?0:1;
         
-        if(nums[0]>nums[1])
-            return 0;
-        if(nums[n-1]>nums[n-2])
-            return n-1;
-        for(int i=1;i<n-1;i++)
+        int start=0;
+        int end=n-1;
+        int mid;
+
+        while(start<end)
         {
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
-                return i;
+            mid=start + (end-start)/2;
+
+            if(nums[mid]>nums[mid+1])
+                end=mid;
+            else
+                start=mid+1;
+            
         }
-        return -1;
+        return start;
     }
 };
