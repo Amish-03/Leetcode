3bb1115971015dbbfa65f17af68638277e6bf0c2
Amish Bhushan Kulkarni <163165606+Amish-03@users.noreply.github.com>
2025-11-19 23:41:39 +0530

    Time: 0 ms (100%), Space: 9.8 MB (21.66%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,15 +1,26 @@
+#include<unordered_map>
 class Solution {
 public:
+
+    
+
+    long long int Nc2(int n)
+    {
+        return (n*(n-1)/2);
+    }
+
     int numIdenticalPairs(vector<int>& nums) {
         int count=0;
+        map<int,int> freq;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i+1;j<nums.size();j++)
+            freq[nums[i]]++;
+        }
+        for(auto cur:freq)
+        {
+            if(cur.second>1)
             {
-                if(nums[i]==nums[j])
-                {
-                    count++;
-                }
+                count = count + Nc2(cur.second);
             }
         }
         return count;
